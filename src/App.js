34c00903f29dade91ff36d7feee82b38d10ddccb
jhanio <jhaniohernandez@outlook.com>
2023-12-02 11:34:23 -0500

    "Se uso funciones para imprimir texto"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function mostrarTitulo(titulo) {
   return (
-    <h1>Hola React!!!!</h1>
+    <h1>
+      {titulo}
+    </h1>
+  );
+}
+function App() {
+  const title = 'LA ODISEA'
+  return (
+    <div>
+      <h1>Hola React!!!!</h1>
+      {mostrarTitulo('TEXTO INGRESADO POR FUNCION')}
+
+      <hr></hr>
+      <h1>El siguiente titulo de libro fue ingresado por una variable :{title}</h1>
+
+    </div>
+
+
+
   );
 }
 
 export default App;
-
